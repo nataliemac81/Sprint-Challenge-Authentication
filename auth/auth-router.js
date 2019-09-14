d5cbffa75327e3476dacbfc6bd0b4fe12df6f509
Natalie McCroy <nataliemccroy@gmail.com>
2019-09-13 20:42:22 -0500

    added generate token function to auth router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+
+const bcrypt = require('bcryptjs');
 
 router.post('/register', (req, res) => {
   // implement registration
@@ -7,5 +10,19 @@ router.post('/register', (req, res) => {
 router.post('/login', (req, res) => {
   // implement login
 });
+
+function generateToken(user) {
+  const payload = {
+    sub: user.id,
+    username: user.username
+  }
+
+  const options = {
+    expiresIn: '1d'
+  }
+
+  return jwt.sign(payload, process.env.JWT_SECRET, options);
+
+}
 
 module.exports = router;
