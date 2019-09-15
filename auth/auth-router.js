48300e14119f04f7b17393b1c47d5da2c943183e
Natalie McCroy <nataliemccroy@gmail.com>
2019-09-15 18:16:00 -0500

    added generate token function to auth router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -10,10 +10,34 @@ router.post('/register', (req, res) => {
   user.password = hash;
 
   Users.add(user)
+  .then(saved => {
+    res.status(201).json(saved);
+  })
+  .catch(error => {
+    res.status(500).json(error);
+  })
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+  .first()
+  .then(user => {
+    if(user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        message: `Welcome ${user.username}!`,
+      });
+    } else {
+      res.status(401).json({
+        message: 'Invalid Credentials'
+      })
+    }
+  })
+  .catch(error => {
+    res.status(500).json(error);
+  })
 });
 
 function generateToken(user) {
